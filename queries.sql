724941d9ae69de26b3d022e9b389363f7ff55530
roma1774 <36560030+roma1774@users.noreply.github.com>
2018-02-17 02:00:31 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,12 +1,12 @@
 use db1;
 
-select 
-libros.id,libros.nombre as titulo,autores.nombre as autor, editoriales.id as editorial
-from
+SELECT
+libros.id,libros.nombre AS titulo,autores.nombre AS autor, editoriales.id AS editorial
+FROM
 libros,editoriales,autores
-where
-libros.id = 'fw_771-h' and
-libros.id_autor = autores.id and
+WHERE
+libros.id = 'fw_771-h' AND
+libros.id_autor = autores.id AND
 libros.id_editorial = editoriales.id;
 
 
